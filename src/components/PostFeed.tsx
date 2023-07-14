5f8d911438d4403e884d5139552b31a5a54f4f85
Alan Martin <alan.edward.martin@gmail.com>
2023-07-13 23:22:38 -0400

    about page

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,21 +1,43 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { FreeMode, Scrollbar } from 'swiper/modules'
+import { FreeMode, Pagination } from 'swiper/modules'
 import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+type Props = {
+  title?: string
+}
 
 
-export default function PostFeed() {
+export default function PostFeed({ title }: Props) {
   return (
     <div
       className='container mx-auto bg-white border-x border-black p-6'
     >
+      {title && <h3 className='text-xl md:text-3xl text-center pb-6'>{title}</h3>}
       <Swiper
-        modules={[FreeMode, Scrollbar]}
-        spaceBetween={24}
-        slidesPerView={3}
-        freeMode={true}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
+        modules={[FreeMode, Pagination]}
+        freeMode
+        pagination={{ clickable: true, dynamicBullets: true, el: '.swiper-pagination' }}
+        breakpoints={{
+          300: {
+            slidesPerView: 'auto',
+            spaceBetween: 24,
+            freeMode: true,
+          },
+
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+            freeMode: true,
+          },
+
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+            freeMode: false,
+          }
+        }}
       >
         <SwiperSlide className='border border-black'>
           <div className='h-[300px]' />
@@ -30,7 +52,10 @@ export default function PostFeed() {
           <div className='h-[300px]' />
         </SwiperSlide>
       </Swiper>
-    </div>
+      <div className='relative pt-10'>
+        <div className="swiper-pagination"></div>
+      </div>
+    </div >
 
   )
 }
