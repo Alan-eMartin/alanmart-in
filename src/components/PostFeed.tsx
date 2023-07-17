54f5dc8ccae8904525c95b25100072ab04c37bd1
Alan Martin <alan.edward.martin@gmail.com>
2023-07-16 23:47:18 -0400

    remove slider all pages but home for now, quick read more link on every article

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -3,13 +3,16 @@ import { FreeMode, Pagination } from 'swiper/modules'
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
+import { Post } from '@/sanity/sanity';
+import { urlForImage } from '@/sanity/urlForImage';
 
 type Props = {
   title?: string
+  posts: Post[]
 }
 
 
-export default function PostFeed({ title }: Props) {
+export default function PostFeed({ title, posts }: Props) {
   return (
     <div
       className='container mx-auto bg-white border-x border-black p-6'
@@ -40,18 +43,28 @@ export default function PostFeed({ title }: Props) {
           }
         }}
       >
-        <SwiperSlide className='border border-black'>
-          <div className='h-[300px]' />
-        </SwiperSlide>
-        <SwiperSlide className='border border-black'>
-          <div className='h-[300px]' />
-        </SwiperSlide>
-        <SwiperSlide className='border border-black'>
-          <div className='h-[300px]' />
-        </SwiperSlide>
-        <SwiperSlide className='border border-black'>
-          <div className='h-[300px]' />
-        </SwiperSlide>
+        {posts.map((post) => (
+          <SwiperSlide className='border border-black'>
+            <a title={`Read: ${post.title}`} href={`/blog/post/${post.slug}`}>
+              <div className='h-[300px] flex items-end justify-center p-6'>
+                <h3
+                  className='relative z-20 text-white text-base md:text-lg mb-1 whitespace-normal line-clamp-2 font-bold text-center'
+                >
+                  {post.title}
+                </h3>
+                <img
+                  className='object-cover h-full w-full absolute inset-0'
+                  src={urlForImage(post.mainImage).url()}
+                  alt={post.mainImage.altText}
+                />
+                <div
+                  className="absolute inset-0 bg-gradient-to-t from-zinc-900 to-transparent z-10"
+                >
+                </div>
+              </div>
+            </a>
+          </SwiperSlide>
+        ))}
       </Swiper>
       <div className='relative pt-10'>
         <div className="swiper-pagination">
